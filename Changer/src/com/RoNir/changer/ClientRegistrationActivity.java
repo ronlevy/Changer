b989b8053093af62ac9a34c33d52a1d99003bc3f
ronlevy <ronlevyy@gmail.com>
2014-12-08 22:57:22 -0800

    refresh

--- Changer/src/com/RoNir/changer/ClientRegistrationActivity.java
+++ Changer/src/com/RoNir/changer/ClientRegistrationActivity.java
@@ -1,7 +1,8 @@
 package com.RoNir.changer;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,7 +17,7 @@ public class ClientRegistrationActivity extends ActionBarActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.client, menu);
+		getMenuInflater().inflate(R.menu.client_registration, menu);
 		return true;
 	}
 
